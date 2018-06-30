4523216eb35e1b9e1b3d07d06c63662e0acf1785
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2018-06-30 13:58:37 +0530

    Add url automation method

---
+++ Experimentation/Automated_url/copy_commands.sh
@@ -0,0 +1,6 @@
+export PATH="/home/soumitra/anaconda3/bin:$PATH"
+cd Working
+wget https://raw.githubusercontent.com/franklinwillemen/Machine_Learning/master/Regression/Simple_Linear_Regression/Salary_Data.csv
+cd
+cd $(jupyter --runtime-dir)
+ls
---
+++ Experimentation/Automated_url/server_commands.sh
@@ -0,0 +1,6 @@
+export PATH="/home/soumitra/anaconda3/bin:$PATH"
+ls
+cd Working
+python preprocessing.py
+cd
+python python_server.py kernel-20409.json linear_regression
