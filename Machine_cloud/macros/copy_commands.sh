678e8f80c8a03ef0b9f4c1173bdda7f3e16d716f
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2018-08-07 19:13:26 +0530

    Mandar sync

--- Machine_cloud/macros/copy_commands.sh
+++ Machine_cloud/macros/copy_commands.sh
@@ -1,4 +1,3 @@
 export PATH="/home/soumitra/anaconda3/bin:$PATH"
-rm attributes.p
 cd $(jupyter --runtime-dir)
 ls
--- Machine_cloud/macros/server_commands.sh
+++ Machine_cloud/macros/server_commands.sh
@@ -1,2 +1,2 @@
 export PATH="/home/soumitra/anaconda3/bin:$PATH"
-python python_server.py kernel-31156.json custom soumitra 1 
+python python_server.py kernel-1360.json predictor.py soumitra 2 
